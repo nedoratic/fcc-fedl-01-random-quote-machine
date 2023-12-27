d1ef78ee127f217f5f96bbce692eba1693275ccf
nedoratic <nedoratic@gmail.com>
2023-12-27 23:36:10 +0100

    added quotes and get random quote function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+import allQuotesData from './assets/allQuotesData.json';
 import './App.css';
 
+interface Quote {
+	text: string;
+	author: string;
+}
+
+const getRandomQuote = (quotesArray: Quote[]): Quote => {
+	const randomIndex = Math.floor(Math.random() * quotesArray.length);
+	return quotesArray[randomIndex];
+};
+
 function App() {
-	return <></>;
+	const [currentQuote] = useState<Quote | null>(getRandomQuote(allQuotesData));
+
+	return (
+		<div>
+			<p>"{currentQuote?.text ?? 'No quote available'}"</p>
+			<p>- {currentQuote?.author ?? 'Unknown author'}</p>
+		</div>
+	);
 }
 
 export default App;
